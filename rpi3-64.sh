97ab69c453c9691567073ad78ba54e5526a9a1ed
Steev Klimaszewski <steev@kali.org>
2019-08-21 14:37:32 -0500

    rpi3-64: Add stanzas for pi2/3/4 for config.txt

--- rpi3-64.sh
+++ rpi3-64.sh
@@ -404,18 +404,22 @@ EOF
 
 # To boot 64bit, these lines *have* to be in config.txt
 cat << EOF >> "${basedir}"/kali-${architecture}/boot/config.txt
+[pi2]
+# Pi2 is 64bit only on v1.2+
+# 64bit kernel for Raspberry Pi 2 is called kernel8 (armv8a)
+kernel=kernel8-alt.img
+[pi3]
+# 64bit kernel for Raspberry Pi 3 is called kernel8 (armv8a)
+kernel=kernel8-alt.img
+[pi4]
+# Enable DRM VC4 V3D driver on top of the dispmanx display stack
+#dtoverlay=vc4-fkms-v3d
+#max_framebuffers=2
+# 64bit kernel for Raspberry Pi 4 is called kernel8l (armv8a)
+kernel=kernel8l-alt.img
+[all]
 # Tell firmware to go 64bit mode.
 arm_64bit=1
-# You can force a device with this, but latest firmware should
-# make the correct choice for dtb
-# RPi3 B dtb file
-#device_tree=bcm2710-rpi-3-b.dtb
-# RPi3 B+ dtb file
-#device_tree=bcm2710-rpi-3-b-plus.dtb
-# RPi3 Compute Module dtb file
-#device_tree=bcm2710-rpi-cm3.dtb
-# 64bit kernel is called kernel8 (armv8a)
-kernel=kernel8-alt.img
 EOF
 
 cp "${basedir}"/../misc/zram "${basedir}"/kali-${architecture}/etc/init.d/zram
