3e10430f2a51cc44668ce5f2a60d0a5fcb518505
Steven Aldinger <stevenaldinger@gmail.com>
2019-12-15 14:14:41 -0500

    add golang installation

--- bootstrap-kali-build-vm.sh
+++ bootstrap-kali-build-vm.sh
@@ -5,16 +5,20 @@ cd /arm
 
 git clone https://gitlab.com/kalilinux/build-scripts/kali-arm /arm/kali-arm
 
+# install image build dependencies
 dpkg --add-architecture i386 && \
 apt-get update && \
 apt-get -y install \
     debootstrap \
     device-tree-compiler \
     git \
-    libncurses5:i386 pixz \
-    lzma lzop u-boot-tools \
+    libncurses5:i386 \
+    lzma \
+    lzop
+    pixz \
     qemu-user-static \
-    systemd-container && \
+    systemd-container \
+    u-boot-tools && \
 mkdir -p arm-stuff/kernel/toolchains && \
 cd /arm/arm-stuff/kernel/toolchains && \
 git clone https://gitlab.com/kalilinux/packages/gcc-arm-eabi-linaro-4-6-2.git
@@ -22,5 +26,11 @@ git clone https://gitlab.com/kalilinux/packages/gcc-arm-eabi-linaro-4-6-2.git
 export ARCH=arm
 export CROSS_COMPILE=/arm/arm-stuff/kernel/toolchains/gcc-arm-eabi-linaro-4.6.2/bin/arm-eabi-
 
+# install golang 1.13
+curl -Lo /tmp/go1.13.5.linux-amd64.tar.gz https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go1.13.5.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
 # cd /arm
-# cd ./kali-arm/rpi.sh 2019.3
+# ./kali-arm/rpi.sh 2019.3
